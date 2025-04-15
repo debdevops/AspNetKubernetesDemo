dcbae0c7d83c7d99c00d5203d2aca221bdaec633
Debasis Ghosh <65386308+debdevops@users.noreply.github.com>
2025-04-15 12:16:02 +0530

    update

--- UsersAPI/obj/Debug/net9.0/UsersAPI.AssemblyInfo.cs
+++ UsersAPI/obj/Debug/net9.0/UsersAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13a8d99dc574612d4d0d7d0e40678e1845a41b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
